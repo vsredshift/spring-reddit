2c0f74514950286334e3dc1653cff0b7908eb5a6
Martin O Shaughnessy <vsredhsift@gmail.com>
2021-09-08 14:20:24 +0200

    Add login and signup routes and logic

--- angular-reddit/src/app/app.module.ts
+++ angular-reddit/src/app/app.module.ts
@@ -1,20 +1,49 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// MODULES
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AuthModule } from './auth/auth.module';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { NgxWebstorageModule } from 'ngx-webstorage'
+import { ToastrModule } from 'ngx-toastr'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { TokenInterceptor } from './token-interceptor';
+
+// COMPONENTS
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+// import { SignupComponent } from './components/auth/signup/signup.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginComponent,
+    HomeComponent,
+    // SignupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    AuthModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NgxWebstorageModule.forRoot(),
+    ToastrModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/main/java/com/vsredshift/springreddit/config/WebConfig.java
@@ -0,0 +1,2 @@
+package com.vsredshift.springreddit.config;public class WebConfig {
+}
